09399cbf70abba6a36bdde32172f5a0c05b2b519
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-08-27 12:00:58 -0400

    working on escape functionality

--- actions/commit.js
+++ actions/commit.js
@@ -2,13 +2,14 @@ const React = require("react");
 const { useState } = require("react");
 const { Box, Text } = require("ink");
 const { execSync } = require("child_process");
-const TextInput = require("ink-text-input").default;
+const TextInput = require("ink-text-input").default
+const goBack = require("ink-text-input")
 
 const CommitAction = (props) => {
 	const [message, setMessage] = useState("");
 
 	let {refreshTab} = props
-
+	goBack(refreshTab(''))
 	const handleSubmit = () => {
 		execSync('git commit -m "' + message +'"')
 		refreshTab('')
